3662f1c2fdee13d7d7085b5c4b08e2679c2d27a8
Kazbonfim <zcry.games@gmail.com>
2024-11-16 02:40:52 -0300

    Primeiros passos, novamente :)

---
+++ app.c
@@ -0,0 +1,13 @@
+// População, área, PIB, Num. de pontos turísticos 
+// Estado (char), Cód. carta (char[]), Nome da cidade (char[]), População (int), Área km2 (float), PIB (float), Pontos turistícos (Int)
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    printf("Olá, mundo!");
+    return 0;
+}
